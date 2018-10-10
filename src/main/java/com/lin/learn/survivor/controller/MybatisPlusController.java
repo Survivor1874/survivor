c32cb22e63bc4c8471a2c37735a701400cec9ca0
Lin1874 <linjun.li@quvideo.com>
2018-10-10 14:19:20 +0800

    redis

--- src/main/java/com/lin/learn/survivor/controller/MybatisPlusController.java
+++ src/main/java/com/lin/learn/survivor/controller/MybatisPlusController.java
@@ -1,8 +1,11 @@
 package com.lin.learn.survivor.controller;
 
+import com.lin.learn.survivor.mapper.UserMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author linjun.li@quvideo.com
@@ -10,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 public class MybatisPlusController {
+
+    @Resource
+    private UserMapper userMapper;
 
     @RequestMapping(path = "/apps", method = RequestMethod.POST)
     public Object getInfo() {
--- src/test/java/com/lin/learn/survivor/SurvivorApplicationTests.java
+++ src/test/java/com/lin/learn/survivor/SurvivorApplicationTests.java
@@ -7,6 +7,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -18,10 +21,21 @@ public class SurvivorApplicationTests {
 	@Autowired
 	private UserMapper userMapper;
 
+	@Autowired
+	private RedisTemplate redisTemplate;
+
 	@Test
 	public void contextLoads() {
 		List<User> users = userMapper.selectList(null);
 		System.out.println(JSON.toJSONString(users));
+	}
+
+	@Test
+	public void redisTest() {
+		redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+		redisTemplate.opsForValue().set("a:b:c", 23407);
+		Object o = redisTemplate.opsForValue().get("a:b:c");
+		System.err.println(o);
 	}
 
 }
